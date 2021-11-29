a5ba3739509283c4c4c9bd76671e874eafade8b5
Farerty <95239369+Farerty@users.noreply.github.com>
2021-11-29 19:45:47 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double theA = 0, theB = 0;
+            
+                try
+                {
+                    
+                    theA = Convert.ToDouble(textBox1.Text);
+                    
+                    theB = Convert.ToDouble(textBox2.Text);
+                    
+                }
+                catch
+                {
+                    MessageBox.Show("Invalid Number");
+                }
+            double theC = 0;
+            theC = Math.Sqrt(theA * theA + theB * theB);
+            textBox3.Text = Convert.ToString(theC);
+        }
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double theA = 0, theB = 0, TheC = 0;
+
+                try
+                {
+                    theA = Convert.ToDouble(textBox1.Text);
+
+                    theB = Convert.ToDouble(textBox2.Text);
+                    
+                    TheC = Convert.ToDouble(textBox3.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Invalid Number");
+                }
+        
+            
+            textBox4.Text = Convert.ToString(resultOfUr(theA, theB, TheC));
+            
+        }
+        public static bool resultOfUr(double a, double b, double c)
+        {
+            double result = b * b - 4 * a * c;
+            if (result <= 0)
+            {
+                return false;
+            }
+            else if (result > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+    }
+}
+
