0a9c47e5e94e26537787f2005df977c333d5ca6b
Farerty <95239369+Farerty@users.noreply.github.com>
2021-12-13 15:23:54 +0300

    Add files via upload

---
+++ WindowsFormsApp16/WindowsFormsApp16/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp16
+{
+    public partial class Form1 : Form
+    {
+        public static TextBox textBox3 = new TextBox();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool f = false;
+            
+            StringBuilder str = new StringBuilder(textBox1.Text);
+            textBox3.Text = "Исходная строка " + str;
+
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i] == str[i + 1])
+                    f = true;
+            }
+            if (f == true)
+                textBox2.Text = "Есть одинаковые символы";
+            else
+                textBox2.Text = "Нет одинаковых символов";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox3.Location = new Point(10, 70);
+            textBox3.Height = 40;
+            textBox3.Width = 195;
+            textBox3.Name = "text_Box3";
+            textBox3.Multiline = true;
+            this.Controls.Add(textBox3);
+           
+        }
+    }
+}
---
+++ WindowsFormsApp18/WindowsFormsApp18/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp18
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = "Компьютер – это электронно-вычислительная машина, способная выполнять заданную последовательность операций, называемую программой. Само слово «компьютер» произошло от английских to compute («вычислять») и computer («вычислитель»).";
+           richTextBox1.Text = str;
+            var newtext = Regex.Replace(str, "[-.?!)(,:]", "");
+            richTextBox2.Text = newtext;
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+        }
+    }
+}
